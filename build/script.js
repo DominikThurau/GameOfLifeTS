89946d6edf9e1599afcdce86ba1fe2d3081e9aa7
Dominik Thurau <dominik.thurau@ueberbit.de>
2021-03-25 17:28:04 +0100

    Refactored

--- build/script.js
+++ build/script.js
@@ -1,15 +1,34 @@
 "use strict";
+/*interface Cell {
+    id: string;
+    x: number;
+    y: number;
+    var objekte = ["sas", "sass", "ff"];
+    let a : Cell = {id: "1", x: 0, y: 0};
+}*/
+//Defining Variables
 var mouseDown = false;
 var isPencil = true;
+var brush = "deafult";
+//Getting Gamefield
+var field = document.getElementById("gamefield");
+//Getting Tags
+var startButton = document.getElementById("start-button"); //Getting Buttons
+var drawModeButton = document.getElementById("draw-mode-button"); //Getting Pencil Button
+//Getting Slider and View of Colum
+var columSlider = document.getElementById("columSlider");
+var columValue = document.getElementById("columValue");
+//Getting Slider and View of Row
+var rowSlider = document.getElementById("rowSlider");
+var rowValue = document.getElementById("rowValue");
+//Toggling if mouse is down
 document.body.onmousedown = function () {
     mouseDown = true;
 };
 document.body.onmouseup = function () {
     mouseDown = false;
 };
-var startButton = document.getElementById("start-button");
-var field = document.getElementById("gamefield");
-var drawModeButton = document.getElementById("draw-mode-button");
+//Changing between Pencil and Eraser
 function toggleDrawMode() {
     isPencil = !isPencil;
     if (isPencil) {
@@ -19,30 +38,24 @@ function toggleDrawMode() {
         drawModeButton ? drawModeButton.innerHTML = "Eraser" : null;
     }
 }
-//Getting Slider and View of Colum
-var columSlider = document.getElementById("columSlider");
-var columValue = document.getElementById("columValue");
-//Getting Slider and View of Row
-var rowSlider = document.getElementById("rowSlider");
-var rowValue = document.getElementById("rowValue");
-startButton && startButton.addEventListener("click", function (e) { return generateField(); });
-var displayColumnValue = function () {
+//Render SliderValues
+function displayColumnValue() {
     var newValue = columSlider.value;
     columValue ? columValue.innerHTML = newValue : null;
     generateField();
-};
-var displayRowValue = function () {
+}
+function displayRowValue() {
     var newValue = rowSlider.value;
     rowValue ? rowValue.innerHTML = newValue : null;
     generateField();
-};
-displayColumnValue();
-displayRowValue();
-//Add Eventlistener
+}
+//Add Eventlistener for  change of Inputvalues
 columSlider.addEventListener("input", displayColumnValue);
 rowSlider.addEventListener("input", displayRowValue);
-var objekte = ["sas", "sass", "ff"];
-var a = { id: "1", x: 0, y: 0 };
+//Init Game
+displayColumnValue();
+displayRowValue();
+//Render gamefield
 function generateField() {
     //clear the field
     field ? field.innerHTML = "" : null;
@@ -57,22 +70,71 @@ function generateField() {
         field ? field.innerHTML += "\n        " + currentRow : null;
     }
 }
+//Bring Cells back to Live or kill them
+function activateCell(xPos, yPos, hover) {
+    var calcId = "x" + xPos + "/y" + yPos;
+    //const clickedCell = document.getElementById(`x${xPos}/y${yPos}`);
+    switch (brush) {
+        case "one": {
+            //statements; 
+            !hover ? brushOne(xPos, yPos) : brushOne(xPos, yPos, true);
+            break;
+        }
+        case "two": {
+            //statements; 
+            break;
+        }
+        default: {
+            !hover ? defaulBrush(xPos, yPos) : defaulBrush(xPos, yPos, true);
+            break;
+        }
+    }
+}
+//Render Selection
 function addSelection(xPos, yPos) {
+    //Getting clicked Position
     var calcId = "x" + xPos + "/y" + yPos;
     var selectedCell = document.getElementById("x" + xPos + "/y" + yPos);
-    selectedCell === null || selectedCell === void 0 ? void 0 : selectedCell.classList.add("mouseOver");
+    activateCell(xPos, yPos, true);
+    //Draw if mouse is down
     if (mouseDown) {
         activateCell(xPos, yPos);
     }
 }
+//Remove Selection
 function removeSelection(xPos, yPos) {
     var calcId = "x" + xPos + "/y" + yPos;
     var selectedCell = document.getElementById("x" + xPos + "/y" + yPos);
-    selectedCell === null || selectedCell === void 0 ? void 0 : selectedCell.classList.remove("mouseOver");
+    activateCell(xPos, yPos, true);
 }
-function activateCell(xPos, yPos) {
-    var calcId = "x" + xPos + "/y" + yPos;
-    var clickedCell = document.getElementById("x" + xPos + "/y" + yPos);
-    var leftCell = document.getElementById("x" + (parseInt(xPos) - 1).toString() + "/y" + (parseInt(yPos) - 1).toString());
-    isPencil ? (clickedCell === null || clickedCell === void 0 ? void 0 : clickedCell.classList.add("alive")) : (clickedCell === null || clickedCell === void 0 ? void 0 : clickedCell.classList.remove("alive"));
+//
+function defaulBrush(xPos, yPos, hover) {
+    //Which cells do you want to change?
+    var cells = [
+        document.getElementById("x" + xPos + "/y" + yPos),
+    ];
+    //If Hovermode toggle hover effect
+    if (hover) {
+        cells.forEach(function (cell) { return cell.classList.toggle("mouse-over"); });
+    }
+    else {
+        cells.forEach(function (cell) { return isPencil ? cell.classList.add("alive") : cell.classList.remove("alive"); });
+    }
+}
+function brushOne(xPos, yPos, hover) {
+    //Which cells do you want to change?
+    var cells = [
+        document.getElementById("x" + xPos + "/y" + yPos),
+        document.getElementById("x" + (parseInt(xPos)).toString() + "/y" + (parseInt(yPos) - 1).toString()),
+        document.getElementById("x" + (parseInt(xPos)).toString() + "/y" + (parseInt(yPos) + 1).toString()),
+        document.getElementById("x" + (parseInt(xPos) - 1).toString() + "/y" + (parseInt(yPos)).toString()),
+        document.getElementById("x" + (parseInt(xPos) + 1).toString() + "/y" + (parseInt(yPos)).toString()),
+    ];
+    //If Hovermode toggle hover effect
+    if (hover) {
+        cells.forEach(function (cell) { return cell.classList.toggle("mouse-over"); });
+    }
+    else {
+        cells.forEach(function (cell) { return isPencil ? cell.classList.add("alive") : cell.classList.remove("alive"); });
+    }
 }
--- script.ts
+++ script.ts
@@ -1,24 +1,42 @@
-interface Cell {
+/*interface Cell {
     id: string;
     x: number;
     y: number;
-}
+    var objekte = ["sas", "sass", "ff"];
+    let a : Cell = {id: "1", x: 0, y: 0};
+}*/
 
+//Defining Variables
 let mouseDown: boolean = false;
 let isPencil: boolean = true;
+let brush: string = "deafult";
 
-document.body.onmousedown = function() { 
-    mouseDown = true;
-  }
-  document.body.onmouseup = function() {
-    mouseDown = false;
-  }
-
-const startButton = document.getElementById("start-button");
+//Getting Gamefield
 const field = document.getElementById("gamefield");
 
-const drawModeButton = document.getElementById("draw-mode-button");
+//Getting Tags
+const startButton = document.getElementById("start-button");//Getting Buttons
+const drawModeButton = document.getElementById("draw-mode-button"); //Getting Pencil Button
 
+//Getting Slider and View of Colum
+const columSlider = document.getElementById("columSlider") as HTMLInputElement;
+const columValue = document.getElementById("columValue");
+
+//Getting Slider and View of Row
+const rowSlider = document.getElementById("rowSlider") as HTMLInputElement;
+const rowValue = document.getElementById("rowValue");
+
+
+//Toggling if mouse is down
+document.body.onmousedown = function() { 
+    mouseDown = true;
+}
+document.body.onmouseup = function() {
+    mouseDown = false;
+}
+
+
+//Changing between Pencil and Eraser
 function toggleDrawMode(){
     isPencil=!isPencil;
 
@@ -30,50 +48,35 @@ function toggleDrawMode(){
     
 }
 
-//Getting Slider and View of Colum
-const columSlider = document.getElementById("columSlider") as HTMLInputElement;
-const columValue = document.getElementById("columValue");
-
-//Getting Slider and View of Row
-const rowSlider = document.getElementById("rowSlider") as HTMLInputElement;
-const rowValue = document.getElementById("rowValue");
-
-
-startButton&&startButton.addEventListener("click", (e:Event) => generateField());
-
-let displayColumnValue = function(){
+//Render SliderValues
+function displayColumnValue(){
     let newValue = columSlider.value;
     columValue?columValue.innerHTML = newValue:null;
     generateField();
 }
-let displayRowValue = function(){
+function displayRowValue(){
     let newValue = rowSlider.value;
     rowValue?rowValue.innerHTML = newValue:null;
     generateField();
 }
 
-displayColumnValue();
-displayRowValue();
-
-//Add Eventlistener
+//Add Eventlistener for  change of Inputvalues
 columSlider.addEventListener("input", displayColumnValue);
 rowSlider.addEventListener("input", displayRowValue);
 
-var objekte = ["sas", "sass", "ff"];
+//Init Game
+displayColumnValue();
+displayRowValue();
 
-
-
-
-let a : Cell = {id: "1", x: 0, y: 0};
-
+//Render gamefield
 function generateField(){
     
     //clear the field
     field?field.innerHTML = "":null;
+
     for(let rowCount = 0; rowCount < parseInt(rowSlider.value); rowCount++ ){
         //Cell "List"
         let cells:string = "";
-
 
         for(let xPos = 0;xPos< parseInt(columSlider.value); xPos++){
             cells += `<div class="cell dead" draggable="false" id="${"x" + xPos +"/y" + rowCount}" data-x="${xPos}" data-y="${rowCount}" dead" onmouseover="addSelection(this.dataset.x, this.dataset.y)" onmouseout="removeSelection(this.dataset.x, this.dataset.y)" onclick="activateCell(this.dataset.x, this.dataset.y)"></div>`;
@@ -89,24 +92,76 @@ function generateField(){
     }
 }
 
+//Bring Cells back to Live or kill them
+function activateCell(xPos:string, yPos:string, hover?:boolean){
+    let calcId = `x${xPos}/y${yPos}`;
+    //const clickedCell = document.getElementById(`x${xPos}/y${yPos}`);
+    switch(brush) { 
+        case "one": { 
+           //statements; 
+           !hover?brushOne(xPos, yPos):brushOne(xPos, yPos, true);
+           break; 
+        } 
+        case "two": { 
+           //statements; 
+           break; 
+        } 
+        default: { 
+            !hover?defaulBrush(xPos, yPos):defaulBrush(xPos, yPos, true);
+           break; 
+        } 
+     } 
+}
+
+//Render Selection
 function addSelection(xPos:string, yPos:string){
+    //Getting clicked Position
     let calcId = `x${xPos}/y${yPos}`;
     const selectedCell = document.getElementById(`x${xPos}/y${yPos}`);
-    selectedCell?.classList.add("mouseOver");
+    activateCell(xPos, yPos, true);
+
+    //Draw if mouse is down
     if(mouseDown){
         activateCell(xPos, yPos);
     }
 }
-
+//Remove Selection
 function removeSelection(xPos:string, yPos:string){
     let calcId = `x${xPos}/y${yPos}`;
     const selectedCell = document.getElementById(`x${xPos}/y${yPos}`);
-    selectedCell?.classList.remove("mouseOver");
+    activateCell(xPos, yPos, true);
 }
 
-function activateCell(xPos:string, yPos:string){
-    let calcId = `x${xPos}/y${yPos}`;
-    const clickedCell = document.getElementById(`x${xPos}/y${yPos}`);
-    const leftCell = document.getElementById(`x${(parseInt(xPos)-1).toString()}/y${(parseInt(yPos)-1).toString()}`);
-    isPencil?(clickedCell?.classList.add("alive")):(clickedCell?.classList.remove("alive"))
+//
+function defaulBrush(xPos:string, yPos:string, hover?:boolean){
+    //Which cells do you want to change?
+    let cells: HTMLElement[] = [
+        document.getElementById(`x${xPos}/y${yPos}`) as HTMLElement,
+    ];
+
+    //If Hovermode toggle hover effect
+    if(hover){
+        cells.forEach(cell => cell.classList.toggle("mouse-over"));
+    } else{
+        cells.forEach(cell => isPencil?cell.classList.add("alive"):cell.classList.remove("alive"))
+    }
 }
+
+function brushOne(xPos:string, yPos:string, hover?:boolean){
+    //Which cells do you want to change?
+    let cells: HTMLElement[] = [
+        document.getElementById(`x${xPos}/y${yPos}`) as HTMLElement,
+        document.getElementById(`x${(parseInt(xPos)).toString()}/y${(parseInt(yPos) -1).toString()}`) as HTMLElement,
+        document.getElementById(`x${(parseInt(xPos)).toString()}/y${(parseInt(yPos)  + 1).toString()}`) as HTMLElement,
+        document.getElementById(`x${(parseInt(xPos) - 1).toString()}/y${(parseInt(yPos)).toString()}`) as HTMLElement,
+        document.getElementById(`x${(parseInt(xPos) + 1).toString()}/y${(parseInt(yPos)).toString()}`) as HTMLElement,
+    ];
+
+    //If Hovermode toggle hover effect
+    if(hover){
+        cells.forEach(cell => cell.classList.toggle("mouse-over"));
+    } else{
+        cells.forEach(cell => isPencil?cell.classList.add("alive"):cell.classList.remove("alive"))
+    }
+}
+
